f6ec25717dfef7e0822b34c879e4fc4cdd409c2a
Antony Roy <47647718+devotta@users.noreply.github.com>
2023-04-10 21:20:24 +0500

    Add files via upload

---
+++ empmgmt/empmgmt/src/main/java/com/gl/empmgmt/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.gl.empmgmt.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.gl.empmgmt.entity.Employee;
+import com.gl.empmgmt.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public Employee getEmployeeById(Long id) {
+		return employeeRepository.findById(id).get();
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	@Override
+	public void deleteEmployeeById(Long id) {
+		employeeRepository.deleteById(id);
+	}
+
+}
